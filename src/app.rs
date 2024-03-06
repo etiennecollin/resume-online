0b28bf90a0fd9c494a3c83e6f712911aff64867b
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 22:58:43 -0500

    Formatting

--- src/app.rs
+++ src/app.rs
@@ -12,8 +12,8 @@ pub fn App() -> impl IntoView {
     view! {
         <div class="grid gap-5 grid-cols-1 lg:grid-cols-5 h-screen">
             <Sidebar/>
-            <div class="lg:col-span-4 flex flex-col flex-auto lg:w-11/12 mx-auto">
-                <div class="flex flex-col flex-auto gap-3 lg:overflow-y-scroll lg:h-0 lg:no-scrollbar pb-4 lg:py-4">
+            <div class="flex flex-col flex-auto mx-auto lg:col-span-4 lg:w-11/12">
+                <div class="flex flex-col flex-auto gap-3 pb-4 lg:h-0 lg:py-4 lg:overflow-y-scroll lg:no-scrollbar">
                     <About/>
                     <Education/>
                     <Experience/>
