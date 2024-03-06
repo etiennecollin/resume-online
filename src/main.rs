fd5a4ec1f3f85f37abc6436cb6704b65b3b77c22
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 23:06:34 -0500

    Fixed background

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     provide_meta_context();
     leptos::mount_to_body(|| {
         view! {
-            <main class="h-screen w-full bg-slate-100 dark:bg-slate-900">
+            <main class="h-screen overflow-y-scroll print:overflow-y-visible w-full bg-slate-100 dark:bg-slate-900">
                 <div class="mx-auto px-12 ">
                     <App/>
                 </div>
