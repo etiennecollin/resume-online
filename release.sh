dee29cc6300016f146b38d0e0d81aec286f3bd9f
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 23:28:49 -0500

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Build the website and add the CNAME file
 trunk build --release -d docs &>/dev/null &&
 	printf "resume.etiennecollin.com" >docs/CNAME &&
-	echo "Website built and CNAME file added" ||
-	echo "Error building the website"
+	echo "Website built and CNAME file created" ||
+	{ echo "Error building the website"; exit 1; }
+
+git add docs &>/dev/null &&
+	git commit -m "Released website" docs &>/dev/null &&
+	echo "Website commited to git" ||
+    { echo "Error commiting website to git"; exit 1; }
