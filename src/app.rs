b596c7734ab4c6226b2ea3af16288c5140375b68
Etienne Collin <collin.etienne.contact@gmail.com>
2024-03-05 22:02:26 -0500

    Fixed spacing

--- src/app.rs
+++ src/app.rs
@@ -13,7 +13,7 @@ pub fn App() -> impl IntoView {
         <div class="grid gap-5 grid-cols-1 lg:grid-cols-5 h-screen">
             <Sidebar/>
             <div class="lg:col-span-4 flex flex-col flex-auto lg:w-11/12 mx-auto">
-                <div class="flex flex-col flex-auto gap-3 lg:overflow-y-scroll lg:h-0 lg:no-scrollbar lg:py-4">
+                <div class="flex flex-col flex-auto gap-3 lg:overflow-y-scroll lg:h-0 lg:no-scrollbar pb-4 lg:py-4">
                     <About/>
                     <Education/>
                     <Experience/>
