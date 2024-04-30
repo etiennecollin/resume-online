b2eb6eeaa4a14c752d865ad5dae6b0bc401249d0
etiennecollin <collin.etienne.contact@gmail.com>
2024-04-29 23:52:30 -0400

    Fixed background

--- src/app.rs
+++ src/app.rs
@@ -10,9 +10,9 @@ use crate::pages::{
 pub fn App() -> impl IntoView {
     provide_meta_context();
     view! {
-        <div class="grid gap-5 grid-cols-1 lg:grid-cols-5 lg:h-screen">
+        <div class="grid gap-5 grid-cols-1 lg:grid-cols-5 min-h-screen">
             <Sidebar/>
-            <div class="flex flex-col flex-auto mx-auto lg:col-span-4 lg:w-11/12">
+            <div class="flex flex-col flex-auto mx-auto lg:col-span-4 w-full lg:w-11/12">
                 <div class="flex flex-col flex-auto gap-3 pb-8 lg:py-8 lg:h-0 lg:overflow-y-scroll lg:no-scrollbar">
                     <About/>
                     <Education/>
